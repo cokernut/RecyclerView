d0afebdfda29c73ffb9ed1f87e54a770d0e2a11a
Cokernut <981100707@qq.com>
2016-10-25 09:54:04 +0800

    自定义TabLayout的tab

--- app/src/main/java/top/cokernut/recyclerview/activity/TypeActivity.java
+++ app/src/main/java/top/cokernut/recyclerview/activity/TypeActivity.java
@@ -7,6 +7,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -59,22 +62,33 @@ public class TypeActivity extends AppCompatActivity {
         mViewPager.setAdapter(mAdapter);
         mTabLayout.setTabMode(TabLayout.MODE_FIXED);
         mTabLayout.setupWithViewPager(mViewPager);
-       /* mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        for (int i = 0; i < mTabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = mTabLayout.getTabAt(i);
+            if (i == 0) {
+                tab.setCustomView(mAdapter.getTabView(i));
+            } else {
+                tab.setCustomView(mAdapter.getDefaultTabView(i));
+            }
+        }
+        mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-
+                tab.setCustomView(null);
+                tab.setCustomView(mAdapter.getTabView(tab.getPosition()));
+                mViewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
+                tab.setCustomView(null);
+                tab.setCustomView(mAdapter.getDefaultTabView(tab.getPosition()));
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
 
             }
-        });*/
+        });
     }
 
     class Model {
@@ -104,7 +118,21 @@ public class TypeActivity extends AppCompatActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return mDatas.get(position).title;
+            //return mDatas.get(position).title;
+            return null;
+        }
+
+        public View getTabView(int position){
+            View view = LayoutInflater.from(TypeActivity.this).inflate(R.layout.item_select_tab, null);
+            TextView tv= (TextView) view.findViewById(R.id.tv_title);
+            tv.setText(mDatas.get(position).title);
+            return view;
+        }
+
+        public View getDefaultTabView(int position){
+            TextView view = (TextView) LayoutInflater.from(TypeActivity.this).inflate(R.layout.item_default_tab, null);
+            view.setText(mDatas.get(position).title);
+            return view;
         }
 
     }
